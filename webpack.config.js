53b428e0284cb8180cd8d9768549e9d4d1889938
zx <zxpstudy@163.com>
2020-11-26 11:48:38 +0800

    feat: init

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const { VueLoaderPlugin } = require("vue-loader");
+
+module.exports = {
+  entry: {
+    app: "./src/index.ts",
+  },
+  output: {
+    filename: "build.js",
+  },
+  resolve: {
+    extensions: [".js", ".ts", ".vue"],
+  },
+  mode: "development",
+  optimization: {},
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+      },
+      {
+        test: /\.(ts|js)$/,
+        loader: "babel-loader",
+      },
+    ],
+  },
+  plugins: [new VueLoaderPlugin()],
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+    compress: true,
+    port: 3000,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+  },
+};
