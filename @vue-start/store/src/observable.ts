8e327c37e0b395d4ede8cce1f3544623ef684467
zx <zxpstudy@163.com>
2020-11-28 18:29:13 +0800

    feat(store): rxjs state 40%

---
+++ @vue-start/store/src/observable.ts
@@ -0,0 +1,67 @@
+import { Observable } from "rxjs";
+import { distinctUntilChanged, map as rxMap } from "rxjs/operators";
+import { shallowEqual } from "./utils";
+import { onMounted, onBeforeUnmount, ref } from "vue";
+
+export type TEqualFn = (a: any, b: any) => boolean;
+
+export interface IMapper<T, TOutput> {
+  (state: T): TOutput;
+
+  equalFn?: TEqualFn;
+}
+
+export class Volume<T, TOutput> extends Observable<TOutput> {
+  static from<T, TOutput>(ob$: Observable<T>, mapper: IMapper<T, TOutput>): Observable<TOutput> {
+    return new Volume<T, TOutput>(ob$, mapper, mapper.equalFn);
+  }
+
+  private _value: TOutput | undefined = undefined;
+
+  get value() {
+    if (typeof this._value === "undefined") {
+      this._value = this.mapper((this.state$ as any).value);
+    }
+    return this._value;
+  }
+
+  constructor(private state$: Observable<T>, private mapper: (state: T) => TOutput, equalFn: TEqualFn = shallowEqual) {
+    super((s) => {
+      return this.state$
+        .pipe(
+          rxMap((state) => {
+            const nextValue = this.mapper(state);
+            if (!equalFn(nextValue, this._value)) {
+              this._value = nextValue;
+            }
+            return this._value;
+          }),
+          distinctUntilChanged(),
+        )
+        .subscribe(s);
+    });
+  }
+}
+
+export const useObservable = <T>(ob$: Observable<T>, defaultValue?: T): T => {
+  const v = ref(defaultValue || (ob$ as any).value);
+  let sub;
+  onMounted(() => {
+    sub = ob$.subscribe((val) => {
+      v.value = val;
+    });
+  });
+  onBeforeUnmount(() => {
+    sub && sub.unsubscribe();
+  });
+
+  return v.value;
+};
+
+export const useConn = <T, TOutput = T>(ob$: Observable<T>, mapper: IMapper<T, TOutput>): Observable<TOutput> => {
+  return Volume.from(ob$, mapper);
+};
+
+export const useSelector = <T, TOutput = T>(ob$: Observable<T>, mapper?: IMapper<T, TOutput>): TOutput => {
+  return useObservable(useConn(ob$, mapper || (((v: T) => v) as any)));
+};
