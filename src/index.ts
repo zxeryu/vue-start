98c58f2e7af9ff20924a14b44b3f22227e436e96
zx <zxpstudy@163.com>
2020-12-16 15:46:59 +0800

    feat(demo): add get config demo

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,10 @@ import { createApp } from "vue";
 import router from "./router";
 import store$ from "./store";
 import App from "./App.vue";
+import { createConfig } from "@vue-start/config";
 import { createRequest } from "@vue-start/request";
 
 const request = createRequest({}, []);
+const config = createConfig();
 
-createApp(App).use(store$).use(router).use(request).mount("#root");
+createApp(App).use(store$).use(router).use(config).use(request).mount("#root");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,19 @@
 <template>
   <div>Home</div>
+  {{ config }}
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import { useConfig } from "@vue-start/config";
 export default defineComponent({
   name: "Home",
+  setup() {
+    const config = useConfig();
+
+    return {
+      config,
+    };
+  },
 });
 </script>
