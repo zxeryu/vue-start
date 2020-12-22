ebb927234cbb432f54a132d33f067bae75d21bcf
zx <zxpstudy@163.com>
2020-12-22 17:53:00 +0800

    perf(core): useEpic

--- @vue-start/store/src/ctx.ts
+++ @vue-start/store/src/ctx.ts
@@ -1,5 +1,5 @@
-import { Store, TData } from "./core";
-import { inject } from "vue";
+import { IEpic, Store, TData } from "./core";
+import { inject, onBeforeUnmount } from "vue";
 
 export const storeKey = "$store";
 
@@ -8,3 +8,12 @@ export const createStore = (initialState: TData): Store => {
 };
 
 export const useStore = <TRoot extends TData = {}>(): Store<TRoot> => inject<Store<TRoot>>(storeKey)!;
+
+export const useEpic = (epic: IEpic) => {
+  const store$ = useStore();
+  const sub = store$.epicOn(epic);
+
+  onBeforeUnmount(() => {
+    sub && sub.unsubscribe();
+  });
+};
