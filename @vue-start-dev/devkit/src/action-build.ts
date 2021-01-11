6ed0f4c81a7d85c6143e37a50bb782cc502a887e
zx <zxpstudy@163.com>
2021-01-11 13:37:18 +0800

    fix(devkit): safeDump to dump

--- @vue-start-dev/devkit/src/action-build.ts
+++ @vue-start-dev/devkit/src/action-build.ts
@@ -2,7 +2,7 @@ import { omitBy, isUndefined } from "lodash";
 import { IState } from "./state";
 import { generate } from "./file";
 import { join } from "path";
-import { safeDump } from "js-yaml";
+import { dump } from "js-yaml";
 import { stringify } from "querystring";
 
 const omitEmpty = <T extends object = any>(o: T) => omitBy(o, (v) => isUndefined(v));
@@ -10,7 +10,7 @@ const omitEmpty = <T extends object = any>(o: T) => omitBy(o, (v) => isUndefined
 export const writeConfig = (cwd: string, state: IState) => {
   generate(
     join(cwd, "./config/default.yml"),
-    safeDump(
+    dump(
       omitEmpty({
         ENV: state.env,
         APP_CONFIG: stringify(state.meta.config || {}, ",", "=", {
