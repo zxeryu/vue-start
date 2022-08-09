6f1dec0e26f0932714b217538619af0bf9e3e4f9
zx <zxpstudy@163.com>
2022-08-09 14:55:11 +0800

    fix(element-pro): export

---
+++ @vue-start/element-pro/src/curd/index.ts
@@ -0,0 +1,8 @@
+export * from "./ctx";
+export * from "./CurdModule";
+export * from "./CurdModuleNet";
+
+export * from "./CurdForm";
+export * from "./CurdList";
+export * from "./CurdDesc";
+export * from "./CurdModule";
--- @vue-start/element-pro/src/index.ts
+++ @vue-start/element-pro/src/index.ts
@@ -1,3 +1,7 @@
-export * from "./field";
 export * from "./form";
 export * from "./table";
+export * from "./curd";
+export * from "./core";
+export * from "./field";
+
+export * from "./util";
