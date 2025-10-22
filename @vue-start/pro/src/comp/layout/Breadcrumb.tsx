c55850c966bda2fdfc2086d932dbe977c7122b3f
zx <zxpstudy@163.com>
2025-10-22 09:51:25 +0800

    feat(pro): layout breadcrumb

---
+++ @vue-start/pro/src/comp/layout/Breadcrumb.tsx
@@ -0,0 +1,13 @@
+import { defineComponent } from "vue";
+
+export const LayoutBreadcrumb = defineComponent({
+  props: {},
+  setup: (props) => {
+
+
+    
+    return () => {
+      return <div class={"pro-layout-breadcrumb"}></div>;
+    };
+  },
+});
